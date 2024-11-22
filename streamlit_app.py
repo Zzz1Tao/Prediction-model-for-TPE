52611b315614b71e07a5ad811cf004fd9f09e238
Zzz1Tao <564935037@qq.com>
2024-11-22 23:12:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,15 +27,10 @@ with st.form("my_form"):
    submitted = st.form_submit_button("Predict")
    if submitted:
       x_train = np.array([[Age,Sex,NCC,Eosinophil,TBAb,ADA,Chloride,Protein,CEA,CA199,CK19,SCC]])
-      explainer = shap.TreeExplainer(
-      model,
-      data=x_train,
-      feature_perturbation="interventional",
-      model_output="probability",
-      )
+      explainer = shap.TreeExplainer(model)
       shap_values = explainer.shap_values(x_train) 
       temp = np.round(x_train, 2)
-      shap.force_plot(explainer.expected_value, shap_values,
+      shap.force_plot(explainer.expected_value[1], shap_values[1],temp,
           feature_names =[Age,Sex,NCC,Eosinophil,TBAb,ADA,Chloride,Protein,CEA,CA199,CK19,SCC], matplotlib=True, show=False)
       # plt.xticks(fontproperties='Times New Roman', size=15)
       # plt.yticks(fontproperties='Times New Roman', size=20)
