8c20ef00c71ed6feb135bf301c9b8ad3056f486e
Zzz1Tao <564935037@qq.com>
2024-11-23 16:28:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ with st.form("my_form"):
     shap_values, 
     temp,
     feature_names = ['Age','Sex','NCC','Eosinophil','TBAb','ADA','Chloride','Protein','CEA','CA199','CK19','SCC'],matplotlib=True,show=False)
+    plt.rcParams['font.size'] = 14 
     plt.xticks(size=15)
     plt.yticks(size=20)
     plt.tight_layout()
