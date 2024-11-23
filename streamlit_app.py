f477bdeb1a7b91f28c8ccb2f92ff5ab898e4f65a
Zzz1Tao <564935037@qq.com>
2024-11-23 16:17:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ with st.form("my_form"):
     plt.xticks(size=15)
     plt.yticks(size=20)
     plt.tight_layout()
-    plt.savefig("TPE force plot.png",dpi=1200)
+    plt.savefig("TPE force plot.png",dpi=600)
     pred = model.predict_proba(x_train)
     st.markdown("#### _Based on feature values, predicted possibility of TPE is {}%_".format(round(pred[0][1], 4)*100))
     st.image('TPE force plot.png')
