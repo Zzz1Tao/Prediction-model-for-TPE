b86387675676b2c23500b9d81ef8f0d0efbab34f
Zzz1Tao <564935037@qq.com>
2024-11-23 15:41:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,11 +37,11 @@ with st.form("my_form"):
     explainer.expected_value, 
     shap_values, 
     temp,
-    feature_names = ['Age','Sex','NCC','Eosinophil','TBAb','ADA','Chloride','Protein','CEA','CA199','CK19','SCC'],matplotlib=True,show=False)
+    feature_names = ['Age','Sex','NCC','Eosinophil','TBAb','ADA','Chloride','Protein','CEA','CA199','CK19','SCC'],matplotlib=True,show=False,link='logit')
     plt.xticks(size=15)
     plt.yticks(size=20)
     plt.tight_layout()
-    plt.savefig("TPE force plot.png",dpi=1200)
+    plt.savefig("TPE force plot.png",dpi=600)
     pred = model.predict_proba(x_train)
     st.markdown("#### _Based on feature values, predicted possibility of TPE is {}%_".format(round(pred[0][1], 4)*100))
     st.image('TPE force plot.png')
