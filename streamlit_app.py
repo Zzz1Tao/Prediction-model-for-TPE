f34f5e4de40e063be45d23a04a426274c4340d30
Zzz1Tao <564935037@qq.com>
2024-11-23 14:54:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,17 @@ with st.form("my_form"):
 
    submitted = st.form_submit_button("Predict")
    if submitted:
-      x_train = np.array([[Age,Sex,NCC,Eosinophil,TBAb,ADA,Chloride,Protein,CEA,CA199,CK19,SCC]])
-      explainer = shap.TreeExplainer(
-      model,
-      data=x_train,
-      feature_perturbation="interventional",
-      model_output="probability",
-      )
-      shap_values = explainer.shap_values(x_train)
-      shap.force_plot(base_value=explainer.expected_value, 
-      shap_values[1], 
-      feature_names = ['Age','Sex','NCC','Eosinophil','TBAb','ADA','Chloride','Protein','CEA','CA199','CK19','SCC'])
+    x_train = np.array([[Age,Sex,NCC,Eosinophil,TBAb,ADA,Chloride,Protein,CEA,CA199,CK19,SCC]])
+    explainer = shap.TreeExplainer(
+    loaded_model,
+    )
+    temp = np.round(x_train, 2)
+    shap_values = explainer.shap_values(x_train) 
+    shap.force_plot(
+    explainer.expected_value, 
+    shap_values, 
+    temp,
+    feature_names = ['Age','Sex','NCC','Eosinophil','TBAb','ADA','Chloride','Protein','CEA','CA199','CK19','SCC'],matplotlib=True)
       plt.xticks(size=15)
       plt.yticks(size=20)
       plt.tight_layout()
